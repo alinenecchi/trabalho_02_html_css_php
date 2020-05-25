ad16a3efd18f5f8ad49d36c62ef8110728d03815
Alanda Leote <alandaleote@gmail.com>
2020-05-25 04:05:29 -0300

    atualizar master

--- php/Usuario.class.php
+++ php/Usuario.class.php
@@ -1,28 +1,14 @@
 <?php
 
-// declaração de classe
 class Usuario {
-
-    // declaração de atributos
-    // encapsulamento:
-    // private = apenas na classe
-    // protected = na classe e nas subclasses
-    // public = aberta
     public $nome;
     public $idade;
     public $peso;
     public $altura;
-		public $categoria;
-		public $imc;
+    public $categoria;
+    public $imc;
 
-
-    // construtor é definido pela palavra reservada __construct
-    // pode ou não ter parâmetros
-    // para um construtor poder não receber parâmetros deve-se
-    // definir valores padrão como neste exemplo
     function __construct($nome, $idade, $peso, $altura, $categoria, $imc) {
-        // associação de valores
-        // usa-se o -> para unir o objeto e o atributo
         $this->nome = $nome;
         $this->idade = $idade;
         $this->peso = $peso;
@@ -31,32 +17,17 @@ class Usuario {
         $this->imc = $imc;
     }
 
-    // "método mágico" para criação de set genérico
-    // ou seja, cria um set que pode ser usado por todos os atributos
     function __set($prop, $val) {
         $this->$prop = $val;
     }
 
-    // "método mágico" para criação de get genérico
-    // ou seja, cria um get que pode ser usado por todos os atributos
     function __get($prop) {
         return $this->$prop;
     }
 
-    // método para impressão de dados do objeto
     function __toString()
     {
-        return "<p>" . $this->nome . ", " .
-            $this->idade . "  anos. Seu IMC é " . $this->imc . " e você está " 
-            . $this->categoria . ". </p><span>Resultado obtido a partir do peso " . $this->peso . 
-						"kg e da altura " . $this->altura . "m.</span>";
+        return "<p>" . $this->nome . ", " . $this->idade . "  anos. Seu IMC é " . $this->imc . " e você está " . $this->categoria . ". </p><span>Resultado obtido a partir do peso " . $this->peso . "kg e da altura " . $this->altura . "m.</span>";
     }
-
 }
-//NOME IDADE anos
-//Seu imc é IMC e você está com CATEGORIA
-//Resultado obtido a partir do peso PESO e da altura ALTURA
-//A média de imc dos internautas é IMC corresponde à CATEGORIA
-//A média de idade é IDADE anos
-
 ?>
